b77a4818f033c27a77a7efb65ebe41dfa6c19fbd
jimfrancois <jimitryfrancois98@gmail.com>
2021-09-07 15:06:17 -0400

    adding Challenge42

--- main.py
+++ main.py
@@ -308,3 +308,12 @@ if num < 10:
 else:
     for i in range(0, 3):
         print("Too high")     
+
+#Challenge42
+total = 0
+for i in range(0, 5):
+    num = int(input("Enter a number: "))
+    ans = input("Do you want this number included? (y/n")
+    if ans == "y":
+        total = total + num
+print(total)         
