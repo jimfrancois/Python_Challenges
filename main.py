906e7468e535e21b2ffb683366cc8d5da1eec08f
jimfrancois <jimitryfrancois98@gmail.com>
2021-09-07 13:17:28 -0400

    adding Challenge34

--- main.py
+++ main.py
@@ -237,3 +237,23 @@ num2 = int(input("Enter another number: "))
 ans1 = num1 // num2
 ans2 = num1 % num2
 print(num1, "divided by", num2, "is", ans1, "with", ans2, "remainng.")
+
+
+#Challenge34
+print('1 Square')
+print('2 Triangle')
+print()
+menuselection = int(input("Enter a number: "))
+if menuselection == 1:
+    side = int(input("Enter the length of one side: "))
+    area = side * side
+    print("The area of your chosen shape is", area)
+
+elif menuselection == 2:
+    base = int(input("Enter the length of the base: ")) 
+    height = int(input("Enter the height of the triangle: "))
+    area = (base * height) / 2
+    print("The area of your chosen shape is", area)
+
+else:
+    print("Incorrect option selected")
