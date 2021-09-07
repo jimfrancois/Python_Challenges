03b845d318f5f9b4c2ac22e72716077a111a9b61
jimfrancois <jimitryfrancois98@gmail.com>
2021-09-07 12:41:57 -0400

    adding Challenge26

--- main.py
+++ main.py
@@ -186,3 +186,15 @@ if len(name) < 5:
     print(name.upper())
 else:
     print(name.lower())
+
+
+#Challenge26
+word = input("Please enter a word: ")
+first = word[0]
+length = len(word)
+rest = word[1:length]
+if first != "a" and first != "e" and first != "i" and first != "u":
+    newword = rest + first + "ay"
+else:
+    newword = word + "way"
+    print(newword.lower()) 
