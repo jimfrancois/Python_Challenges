e6b7f450ebb3aae4539b3987c96b3ded3d9afeb5
jimfrancois <jimitryfrancois98@gmail.com>
2021-09-07 15:10:51 -0400

    adding Challenge44

--- main.py
+++ main.py
@@ -334,7 +334,14 @@ else:
     print("I don't understand") 
 
 
-
+#Challenge44
+num = int(input("How many friends do you want to invite to the party? "))
+if num < 10:
+    for i in range(0, num):
+        name = input("Enter a name: ")
+        print(name, "has been invited")
+else:
+    print("Too many people") 
 
 
 
