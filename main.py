c72d125450249953587c85f5ea7a6c8e30582df9
jimfrancois <jimitryfrancois98@gmail.com>
2021-09-07 15:08:43 -0400

    adding Challenge43

--- main.py
+++ main.py
@@ -316,4 +316,29 @@ for i in range(0, 5):
     ans = input("Do you want this number included? (y/n")
     if ans == "y":
         total = total + num
-print(total)         
+print(total) 
+
+
+#Challenge43
+
+direction = input("Do you want to count up or down? (u/d) ")
+if direction == "u":
+    num = int(input("What is the top number? "))
+    for i in range(1, num + 1):
+        print(i)
+elif direction == "d":
+    num = int(input("Enter a number below 20: "))
+    for i in range(20, num - 1, -1 ):
+        print(i)
+else:
+    print("I don't understand") 
+
+
+
+
+
+
+
+
+
+
