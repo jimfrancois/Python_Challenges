fe3de5e482c19a59dbe6940dfd8b7e0699935195
jimfrancois <jimitryfrancois98@gmail.com>
2021-09-07 15:02:35 -0400

    adding Challenge40

--- main.py
+++ main.py
@@ -292,3 +292,9 @@ num = int(input("Enter a number between 1 and 12: "))
 for i in range(1, 13):
     answer = i * num
     print(i, "x", num, "=", answer)
+
+
+#Challenge40
+num = int(input("Enter a number below 50: "))
+for i in range(50, num-1, -1):
+    print(i)    
