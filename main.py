af04f51e53e98ff2b2b091b99c490c25db20d980
jimfrancois <jimitryfrancois98@gmail.com>
2021-09-07 12:30:01 -0400

    adding Challenge17

--- main.py
+++ main.py
@@ -108,3 +108,15 @@ if raining == "yes":
         print("Take an umbrella")
 else:
     print("Enjoy your day")     
+
+
+#Challenge17
+age = int(input("What is your age? "))
+if age >= 18:
+    print("You can vote")
+elif age == 17:
+    print("You can learn to drive")
+elif age == 16:
+    print("You can buy a lottery ticket") 
+else:
+    print("You can gpo Trick-or-Treating")     
