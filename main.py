4bb3e527fa16b3c9d2bf40db501672083b46d9f2
jimfrancois <jimitryfrancois98@gmail.com>
2021-09-07 14:59:02 -0400

    adding Challenge38

--- main.py
+++ main.py
@@ -278,4 +278,10 @@ name = input("Enter your name: ")
 for i in name:
     print(i)   
     
-     
+
+#Challenge38
+num = int(input("Enter a number: "))
+name = input("Enter your name: ")
+for x in range(0, num):
+    for i in name:
+        print(i)     
