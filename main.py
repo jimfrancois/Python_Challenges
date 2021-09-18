d582dc346faecc76ff476cc3b7e3484cad4d4a7f
jimfrancois <jimitryfrancois98@gmail.com>
2021-09-18 12:47:48 -0400

    adding Challenge72

--- main.py
+++ main.py
@@ -735,3 +735,15 @@ sports_list = ["tennis", "footbal"]
 sports_list.append(input("What is your favourite sport? "))
 sports_list.sort()
 print(sports_list)
+
+#Challenge72
+subject_list = ["maths", "engelish", "computing", "history", "science", "spanish"]
+
+print(subject_list)
+dislike = input("Which of these subjects do you dislike? ")
+
+getrid = subject_list.index(dislike)
+del subject_list[getrid]
+print(subject_list)
+
+
