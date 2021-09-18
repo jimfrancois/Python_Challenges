d35b753236a5215ef0798e861497712dbae8a10e
jimfrancois <jimitryfrancois98@gmail.com>
2021-09-18 13:02:36 -0400

    adding Challenge80

--- main.py
+++ main.py
@@ -868,3 +868,14 @@ if lastnum == "n":
 print(num) 
 
 
+#Challenge80
+
+fname = input("Enter your first name: ")
+print("That has", len(fname), "characters in it")
+
+sname = input("Enter your surname: ")
+print("That has", len(sname), "characters in it")
+
+name = fname + " " + sname
+print("Your full name is", name)
+print("That has", len(name), "characters in it")
