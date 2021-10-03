5487149e84340becc5dd6bbe54336baa805326ae
jimfrancois <jimitryfrancois98@gmail.com>
2021-10-03 14:19:17 -0400

    adding Challenge113

--- main.py
+++ main.py
@@ -1305,3 +1305,31 @@ file = open("Books.csv", "r")
 for row in file:
     print(row)
 file.close()
+
+
+#Challenge113
+num = int(input("How many books do you want to add to the list? "))
+file = open("Books.csv", "a")
+
+for x in range(0, num):
+    title = input("Entyer a title: ")
+    author = input("Enter an author: ")
+    year = input("Enter the year it was released: ")
+
+    newrecord = title + "," + author + "," + year + "\n"
+    file.write(str(newrecord))
+file.close()
+
+searchauthor = input("Enter an authors name to search for: ")
+
+file = open("Books.csv", "r")
+count = 0
+
+for row in file:
+    if searchauthor in str(row):
+        print(row)
+        count = count + 1
+if count == 0:
+    print("There are no books by that author in this list.")
+file.close()   
+
