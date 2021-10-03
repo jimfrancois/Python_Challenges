5e0e38967f9ec1f854507f5750cdd2c98dfa663a
jimfrancois <jimitryfrancois98@gmail.com>
2021-10-03 14:12:58 -0400

    adding Challenge109

--- main.py
+++ main.py
@@ -1225,5 +1225,29 @@ file = open("Names.txt", "r")
 print(file.read())
 file.close()
 
+#Challenge109
+print("1) Create a new file")
+print("2) Display the file")
+print("3)Add a new item to the file")
 
+choose = int(input("Make a selection 1, 2 or 3: "))
+if choose == 1:
+    subject = input("Enter a school subject: ")
+    file = open("Subject.txt", "w")
+    file.write(subject + "\n")
+    file.close()
 
+elif choose == 2:
+    file = open("Subject.txt", "r")
+    print(file.read())
+
+elif choose == 3:
+    file = open("Subject.txt", "a")
+    subject = input("Enter a school subject: ")
+    file.write(subject + "\n")
+    file.close()
+    file = open("Subject.txt", "r")
+    print(file.read())
+
+else:
+    print("Invalid option")
