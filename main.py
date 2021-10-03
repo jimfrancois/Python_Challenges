42a125bc921d3417dc5f648b1f0ab794458b2c9f
jimfrancois <jimitryfrancois98@gmail.com>
2021-10-03 14:03:18 -0400

    adding Challenge101

--- main.py
+++ main.py
@@ -1131,9 +1131,23 @@ sales = {
 }
 
 
+#Challenge101
 
 
+sales = {
+    "John": {"N": 3056, "S": 8463, "E": 8441, "W": 2694},
+    "Tom": {"N": 4832, "S": 6786, "E": 4737, "W": 3612},
+    "Anne": {"N": 5239, "S": 4802, "E": 5820, "W": 1859},
+    "Fiona": {"N": 3904, "S": 3645, "E": 8821, "W": 2451},
+}
 
+person_name = input("Enter a sales person's name: ")
+region = input("Enter a sales person's region: ")
+print(sales[person_name][region])
+
+new_data = int(input("Enter new data: "))
+sales[person_name][region] = new_data
+print(sales[person_name])
 
 
 
