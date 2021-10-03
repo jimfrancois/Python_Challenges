46547ae474a7ff79215f3a5670a3fb90fdf5f243
jimfrancois <jimitryfrancois98@gmail.com>
2021-10-03 14:23:04 -0400

    adding Challenge116

--- main.py
+++ main.py
@@ -1359,4 +1359,44 @@ for row in file:
     print(display)
     x = x + 1   
 
+#Challenge116
+file = list(csv.reader(open("Books.csv")))
+Booklist = []
+
+for row in file:
+    Booklist.append(row)
     
+x = 0
+for row in Booklist:
+    display = x, Booklist[x]
+    print(display)
+    x = x + 1
+getrid = int(input("Enter a row number to delete: "))
+del Booklist[getrid]
+
+
+x = 0
+for row in Booklist:
+    display = x, Booklist[x]
+    print(display)
+    x = x + 1
+alter = int(input("Enter a row number to alter: "))
+
+
+x = 0
+for row in Booklist[alter]:
+    display = x, Booklist[alter][x]
+    print(display)
+    x = x + 1
+part = int(input("Which part do you want to change? "))
+newdata = input("Enter new data: ")
+Booklist[alter][part] = newdata
+print(Booklist[alter])
+
+file = open("Books.csv", "w")
+x = 0
+for row in Booklist:
+    newrecord = Booklist[x][0] + ", " + Booklist[1] + ", " + Booklist[x][2] + "\n"
+    file.write(newrecord)
+    x = x + 1
+file.close()   
