b9732d189c030341ea0b8eba4483a2ed4860d330
jimfrancois <jimitryfrancois98@gmail.com>
2021-10-03 14:27:48 -0400

    adding Challenge120

--- main.py
+++ main.py
@@ -1476,4 +1476,49 @@ def main():
     guess = first_guess()
     check_answer(comp_num, guess)
     
-main()    
+main() 
+
+
+#Challenge120
+def addition():
+    num1 = random.randint(5, 20)
+    num2 = random.randint(5, 20)
+    print(num1, "+", num2, "= ")
+    user_answer = int(input("Your answer: "))
+    actual_answer = num1 + num2
+    answers = (user_answer, actual_answer)
+    return answers
+
+def substraction():
+    num3 = random.randint(5, 20)
+    num4 = random.randint(5, 20)
+    print(num3, "-", num4, "= ")
+    user_answer = int(input("Your answer: "))
+    actual_answer = num3 - num4
+    answers = (user_answer, actual_answer)
+    return answers
+
+def check_answer(user_answer, actual_answer):
+    if user_answer == actual_answer:
+        print("Correct")
+        
+    else:
+        print("Incorrect, the answer is", actual_answer)
+        
+def main():
+    print("1) Addition")
+    print("2) Substraction")
+    selection = int(input("Enter 1 or 2: "))
+    
+    if selection == 1:
+        user_answer, actual_answer = addition()
+        check_answer(user_answer, actual_answer)
+
+    elif selection == 2:
+        user_answer, actual_answer = addition()
+        check_answer(user_answer, actual_answer)
+        
+    else:
+        print("Incorrect seelection")
+    
+main()     
