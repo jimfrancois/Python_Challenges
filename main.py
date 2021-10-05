6411a6cb5762cfa62195cd123629e951508f3fcd
jimfrancois <jimitryfrancois98@gmail.com>
2021-10-04 21:34:28 -0400

    adding Challenge123

--- main.py
+++ main.py
@@ -1590,7 +1590,6 @@ main()
 
 #Challenge122
 
-
 def addtofile():
     file = open("Salaries.csv", "a")
     name = input("Enter a name: ")
@@ -1625,3 +1624,62 @@ while tryagain == True:
         
     else:
         print("Incorrect option")
+
+
+#Challenge123
+
+def addtofile():
+    file = open("Salaries.csv", "a")
+    name = input("Enter a name: ")
+    salary = int(input("Enter Salary: "))
+    newrecord = name + ", " + str(salary) + "\n"
+    file.write(str(newrecord))
+    file.close()
+
+
+def viewrecords():
+    file = open("Salaries.csv", "r")
+    for row in file:
+        print(row)
+    file.close()
+
+def  deleterecord():
+    file = open("Salaries.csv", "r")
+    x = 0
+    tmplist = []
+    for row in file:
+        tmplist.append(row)
+    file.close()
+    
+    for row in tmplist:
+        print(x, row)
+        x = x + 1
+    rowtodelete = int(input("Enter the row number to delete: "))
+    del tmplist[rowtodelete]
+    file = open("Salaries.csv", "w")
+    for row in tmplist:
+        file.write(row)
+    file.close()    
+        
+tryagain = True
+while tryagain == True:
+    print("1) Add to file")
+    print("2) View all records")
+    print("3) Quit program")
+    print()
+
+    selection = input("Enter the number of your selection: ")
+    if selection == 1:
+        addtofile()
+
+    elif selection == 2:
+        viewrecords()
+
+    elif selection == 3:
+        tryagain = False
+
+    else:
+        print("Incorrect option")
+
+        
+        
